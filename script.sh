6c533d0b8343988ce5be334e0ef161c27f6c4365
lgamez-kemok <77128151+lgamez-kemok@users.noreply.github.com>
2021-02-03 13:11:19 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,10 +2,25 @@
 
 apt update
 
-apt install openjdk-8-jre-headless && y
+if [ $(dpkg-query -W -f='${Status}' openjdk-8-jre-headless 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt install openjdk-8-jre-headless && y
+fi
 
-mkdir -p /opt/metabase && cd /opt/metabase
+MB_FOLDER=/opt/metabase
 
-wget https://downloads.metabase.com/v0.37.8/metabase.jar
+if [ ! -d "$MB_FOLDER" ]; then
+  mkdir -p /opt/metabase && cd /opt/metabase
+else
+  echo "El directorio $MB_FOLDER ya existe"
+fi
+
+MB_FILE=/opt/metabase/metabase.jar
+
+if [ ! -f "$MB_FILE" ]; then
+  wget https://downloads.metabase.com/v0.37.8/metabase.jar
+else
+  echo "El archivo $MB_FILE ya existe"
+fi
 
 echo "Proceso finalizado"
